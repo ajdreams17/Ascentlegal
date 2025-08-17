4acd6352395ca9367d26d8e7d1bb4caa244a457b
ajdreams17 <ajphomes17@gmail.com>
2025-08-16 23:38:00 -0700

    Create page.tsx

---
+++ app/attorneys/page.tsx
@@ -0,0 +1,8 @@
+export default function AttorneysPage() {
+  return (
+    <main className="mx-auto max-w-5xl p-6">
+      <h1 className="text-2xl font-semibold">Our Attorneys</h1>
+      {/* move your HTML here as JSX */}
+    </main>
+  );
+}

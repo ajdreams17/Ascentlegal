fe5c66d81ad3102db8461298952a18b54228fc16
ajdreams17 <ajphomes17@gmail.com>
2025-08-23 16:40:18 -0700

    Update TrustpilotBadge.jsx

--- components/TrustpilotBadge.jsx
+++ components/TrustpilotBadge.jsx
@@ -1,20 +1,20 @@
+// components/TrustpilotBadge.jsx
 "use client";
 import Script from "next/script";
 
 export default function TrustpilotBadge() {
   return (
     <>
-      {/* Load Trustpilot script */}
       <Script
         id="trustpilot-bootstrap"
         strategy="afterInteractive"
         src="https://widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js"
       />
-
-      <div className="trustpilot-widget"
+      <div
+        className="trustpilot-widget"
         data-locale="en-US"
-        data-template-id="5419b6a8b0d04a076446a9ad"   // TrustScore badge
-        data-businessunit-id="68aa42eff868ed4fa17d7bfb" // <- your ID
+        data-template-id="5419b6a8b0d04a076446a9ad"
+        data-businessunit-id="68aa42eff868ed4fa17d7bfb"
         data-style-height="36px"
         data-style-width="100%"
         data-theme="light"

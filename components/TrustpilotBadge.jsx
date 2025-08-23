71a46ddee3f911ff9a2cfd388d4e7e747b08f12e
ajdreams17 <ajphomes17@gmail.com>
2025-08-23 16:33:55 -0700

    Create TrustpilotBadge.jsx

---
+++ components/TrustpilotBadge.jsx
@@ -0,0 +1,32 @@
+"use client";
+import Script from "next/script";
+
+export default function TrustpilotBadge() {
+  return (
+    <>
+      {/* Load Trustpilot script */}
+      <Script
+        id="trustpilot-bootstrap"
+        strategy="afterInteractive"
+        src="https://widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js"
+      />
+
+      <div className="trustpilot-widget"
+        data-locale="en-US"
+        data-template-id="5419b6a8b0d04a076446a9ad"   // TrustScore badge
+        data-businessunit-id="68aa42eff868ed4fa17d7bfb" // <- your ID
+        data-style-height="36px"
+        data-style-width="100%"
+        data-theme="light"
+      >
+        <a
+          href="https://www.trustpilot.com/review/ascentlegal.io"
+          target="_blank"
+          rel="noopener"
+        >
+          Trustpilot
+        </a>
+      </div>
+    </>
+  );
+}

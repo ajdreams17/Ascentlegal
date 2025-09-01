5382836fad5ae994e924c803139614d24e62c216
ajdreams17 <ajphomes17@gmail.com>
2025-09-01 14:21:27 -0700

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -62,7 +62,7 @@ export default function Header() {
           onClick={() => setOpen(true)}
           className="md:hidden h-10 w-10 flex items-center justify-center rounded-lg border border-gray-200"
         >
-          <svg width="20" height="20" viewBox="0 0 24 24">
+          <svg width="200" height="20" viewBox="0 0 24 24">
             <path d="M4 6h16M4 12h16M4 18h16" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
           </svg>
         </button>

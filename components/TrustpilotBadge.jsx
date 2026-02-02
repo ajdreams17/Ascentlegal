c96a5adb5b88733a83c42a12cc0b747c13fb8238
ajdreams17 <ajphomes17@gmail.com>
2026-02-01 20:21:24 -0800

    Update TrustpilotBadge.jsx

--- components/TrustpilotBadge.jsx
+++ components/TrustpilotBadge.jsx
@@ -58,13 +58,13 @@ export default function TrustpilotBadge() {
       <div
         className="trustpilot-widget"
         data-locale="en-US"
-        data-template-id="YOUR_TEMPLATE_ID"
-        data-businessunit-id="YOUR_BUSINESS_UNIT_ID"
+        data-template-id="5419b6a8b0d04a076446a9ad"
+        data-businessunit-id="68aa42eff868ed4fa17d7bfb"
         data-style-height="52px"
         data-style-width="100%"
         data-theme="light"
       >
-        <a href="https://www.trustpilot.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.trustpilot.com/review/ascentlegal.io" target="_blank" rel="noopener noreferrer">
           Trustpilot
         </a>
       </div>
